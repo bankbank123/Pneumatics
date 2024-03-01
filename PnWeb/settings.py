5a2e18311c1c7e553d6b995a1b6211bcb06f97fb
bankbank123 <chidchanun.bank@mail.kmutt.ac.th>
2024-03-02 01:33:38 +0700

    second4

--- PnWeb/settings.py
+++ PnWeb/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-c%ffg=4ztje6&7-t3z&2=6-(=z3^jl*4ncf^!1-(33$5nb60-1'
 DEBUG = True
 
 # SECURITY WARNING: don't run with debug turned on in production!
